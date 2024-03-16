01d9aa7367533dbafc6c4b9743203f900818e815
GregoryLebret-STW <gregory.lebret@severntrent.co.uk>
2024-03-16 11:48:17 +0000

    remove features

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,5 @@
 export const navigation = [
   { label: 'Home', href: '#home' },
-  { label: 'Features', href: '#features' },
   { label: 'Demos', href: '#demos' },
   { label: 'Documentation', href: '#documentation' },
 ];

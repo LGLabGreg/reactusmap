b05e74130a1a8b72e4a8205b2d7c32d8e10f2ff5
GregoryLebret-STW <gregory.lebret@severntrent.co.uk>
2024-03-16 14:39:23 +0000

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { DM_Sans, Roboto_Mono } from 'next/font/google';
 
 import MainHeader from '@/app/main-header';
@@ -50,6 +51,7 @@ export default function RootLayout({
         <MainHeader />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
